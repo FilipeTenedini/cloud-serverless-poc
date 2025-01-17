ce2aa131ffc44cbb3a5b236633e1b9c4569ead5f
FilipeTenedini <tenedinifilipe@gmail.com>
2025-01-17 09:24:24 -0300

    feat: create products table

--- lib/products-app-stack.ts
+++ lib/products-app-stack.ts
@@ -1,15 +1,32 @@
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as lambdaNodeJs from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export class ProductsAppStack extends cdk.Stack {
 	readonly productsFetchHandler: lambdaNodeJs.NodejsFunction;
+	readonly productsDb: dynamodb.Table;
 
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
 
 		// The code that defines your stack goes here
+
+		this.productsDb = new dynamodb.Table(this, 'ProductsTable', {
+			tableName: 'products',
+			/* esse recurso o padrão é que não seja destruído ao rodar um destroy para não perder os dados.
+            nesse momento deixaremos como destroy para facilitar a limpeza do ambiente.*/
+			removalPolicy: cdk.RemovalPolicy.DESTROY,
+			partitionKey: {
+				name: 'id',
+				type: dynamodb.AttributeType.STRING,
+			},
+			billingMode: dynamodb.BillingMode.PROVISIONED,
+			readCapacity: 1, // QUANTAS REQUISIÇÕES POR SEGUNDO
+			writeCapacity: 1, // QUANTAS REQUISIÇÕES POR SEGUNDO
+		});
+
 		this.productsFetchHandler = new lambdaNodeJs.NodejsFunction(this, 'ProductsFetchFunction', {
 			runtime: lambda.Runtime.NODEJS_20_X,
 			functionName:'ProductsFetchFunction',
