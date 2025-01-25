6b4bb4fad46e36d3b0602794eedd04e2a0706390
FilipeTenedini <tenedinifilipe@gmail.com>
2025-01-25 11:05:11 -0300

    feat: use repository methods and implement error handler

--- lambda/products/products-fetch-function.ts
+++ lambda/products/products-fetch-function.ts
@@ -1,6 +1,15 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import { ProductRepository } from '/opt/nodejs/products-layer';
+import  { DynamoDB } from 'aws-sdk';
+import { errorHandler } from 'error/error-handler';
+import { NotFoundException } from 'error/exceptions/not-found.excepetion';
+import { BadRequestException } from 'error/exceptions/bad-request.exception';
 
-export async function handler(event: APIGatewayProxyEvent, ctx: Context): Promise<APIGatewayProxyResult> {
+const productsDb = process.env.PRODUCTS_DB as string;
+const ddbClient = new DynamoDB.DocumentClient();
+const productRepository = new ProductRepository(ddbClient, productsDb);
+
+export const handler = errorHandler(async (event: APIGatewayProxyEvent, ctx: Context): Promise<APIGatewayProxyResult> => {
 	const { httpMethod, requestContext: { requestId: apiRequestId } } = event;
 	const { awsRequestId: lambdaRequestId } = ctx;
 
@@ -8,30 +17,30 @@ export async function handler(event: APIGatewayProxyEvent, ctx: Context): Promis
 
 	if (event.resource === '/products' && httpMethod === 'GET') {
 		console.log('GET /products');
+		const products = await productRepository.getAll();
 
 		return {
 			statusCode: 200,
-			body: JSON.stringify({
-				message: 'GET /products succefully returned (hello cdk)'
-			})
+			body: JSON.stringify(products)
 		};
 	} else if (event.resource === '/products/{id}') {
 		const productId = event.pathParameters?.id as string;
 		console.log(`GET /products/{id} called with ${productId}`);
 
+		const product = await productRepository.findById(productId);
+
+		// TODO: caso o repository retorne um erro, o handler deve tratar o erro e retornar uma resposta adequada e nao 404
+
+		if (!product) {
+			throw new NotFoundException(`Product ${productId} not found`, apiRequestId);
+		}
+
 		return {
 			statusCode: 200,
-			body: JSON.stringify({
-				message: `GET /products/{id} called with ${productId}`
-			})
+			body: JSON.stringify(product)
 		};
 	}
 
-	return {
-		statusCode: 400,
-		body: JSON.stringify({
-			message: 'Bad Request'
-		})
-	};
+	throw new BadRequestException(`Endpoint ${event.resource} not found`, apiRequestId);
 
-}
+});
