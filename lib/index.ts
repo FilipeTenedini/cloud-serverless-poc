4e992934ff03a7906b18f9eb5d214883f50b8841
FilipeTenedini <tenedinifilipe@gmail.com>
2025-01-26 16:07:34 -0300

    feat: rename files (#18)

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
-export * from './ecommerce-api-stack';
-export * from './products-app-stack';
-export * from './product-app-layers-stack';
+export * from './ecommerce-api.stack';
+export * from './products-app.stack';
+export * from './product-app-layers.stack';
 export * from './events-db.stack';
