05f9907558b8b430362728adb7bf6fba0c9b1b98
FilipeTenedini <tenedinifilipe@gmail.com>
2025-01-26 12:26:31 -0300

    feat: create event layer

--- lib/product-app-layers-stack.ts
+++ lib/product-app-layers-stack.ts
@@ -4,12 +4,10 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as ssm from 'aws-cdk-lib/aws-ssm';
 
 export class ProductAppLayersStack extends cdk.Stack {
-	readonly productsLayer: lambda.LayerVersion;
-
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
 
-		this.productsLayer = new lambda.LayerVersion(this, 'ProductsLayer', {
+		const productsLayer = new lambda.LayerVersion(this, 'ProductsLayer', {
 			code: lambda.Code.fromAsset('lambda/products/layers/products-layer'),
 			compatibleRuntimes: [lambda.Runtime.NODEJS_20_X, lambda.Runtime.NODEJS_LATEST],
 			layerVersionName: 'ProductsLayer',
@@ -21,7 +19,23 @@ export class ProductAppLayersStack extends cdk.Stack {
 
 		new ssm.StringParameter(this, 'ProductsLayerVersionArn', {
 			parameterName: 'ProductsLayerVersionArn',
-			stringValue: this.productsLayer.layerVersionArn
+			stringValue: productsLayer.layerVersionArn
+		});
+
+		const productsEventsLayer = new lambda.LayerVersion(this, 'ProductsEventsLayer', {
+			code: lambda.Code.fromAsset('lambda/products/layers/products-events-layer'),
+			compatibleRuntimes: [lambda.Runtime.NODEJS_20_X, lambda.Runtime.NODEJS_LATEST],
+			layerVersionName: 'ProductsEventsLayer',
+			// por padrão ao destruir essa stack o lambda layer também será destruído
+			// aqui nós iremos manter o lambda layer mesmo após a destruição da stack
+			// porque pode ser que a gente esteja usando esse layer em outra stack
+			removalPolicy: cdk.RemovalPolicy.RETAIN,
+
+		});
+
+		new ssm.StringParameter(this, 'ProductsEventsLayerVersionArn', {
+			parameterName: 'ProductsEventsLayerVersionArn',
+			stringValue: productsEventsLayer.layerVersionArn
 		});
 	}
 }
