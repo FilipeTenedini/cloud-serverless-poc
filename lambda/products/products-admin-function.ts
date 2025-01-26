98d6ff6a30bc73ddb1e99b6a4f1f6b0ff2930288
FilipeTenedini <tenedinifilipe@gmail.com>
2025-01-26 14:51:10 -0300

    Feature/create event handler (#16)
    * feat: create product-events-function
    
    * feat: export event types
    
    * feat: create sendEventFunction and use on endpoints
    
    * feat: type Item on create event function
    
    * feat: add todo

--- lambda/products/products-admin-function.ts
+++ lambda/products/products-admin-function.ts
@@ -1,16 +1,18 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import { ProductRepository } from '/opt/nodejs/products-layer';
-import  { DynamoDB } from 'aws-sdk';
 import { errorHandler } from 'error/error-handler';
-import {  } from 'aws-cdk-lib/aws-lambda';
 import { NotFoundException } from 'error/exceptions/not-found.excepetion';
-import { Product } from 'types/product';
+import { Product, ProductEventType } from 'types/product';
+import { ProductEvent } from 'types/product';
 import * as AWSXRay from 'aws-xray-sdk';
 import * as AWS from 'aws-sdk';
+const { DynamoDB, Lambda } = AWS;
 
 AWSXRay.captureAWS(AWS);
 const productsDb = process.env.PRODUCTS_DB as string;
 const ddbClient = new DynamoDB.DocumentClient();
+const productEventsFunctionName = process.env.PRODUCTS_EVENTS_FUNCTION_NAME as string;
+const lambdaClient = new Lambda();
 const productRepository = new ProductRepository(ddbClient, productsDb);
 
 export const handler =  errorHandler(async (event: APIGatewayProxyEvent, ctx: Context): Promise<APIGatewayProxyResult> => {
@@ -24,6 +26,9 @@ export const handler =  errorHandler(async (event: APIGatewayProxyEvent, ctx: Co
 		const product = JSON.parse(event.body!) as Product;
 
 		const productCreated = await productRepository.create(product);
+
+		const eventResponse = await sendProductEvent(productCreated, 'email@email.com.br', lambdaRequestId, ProductEventType.CREATED);
+		console.log(`Event response: ${eventResponse}`);
 
 		return {
 			statusCode: 200,
@@ -40,6 +45,9 @@ export const handler =  errorHandler(async (event: APIGatewayProxyEvent, ctx: Co
 			try {
 				const productUpdated = await productRepository.update(productId, product);
 
+				const eventResponse = await sendProductEvent(productUpdated, 'email2@email2.com.br', lambdaRequestId, ProductEventType.UPDATED);
+				console.log(`Event response: ${eventResponse}`);
+
 				return {
 					statusCode: 200,
 					body: JSON.stringify(productUpdated)
@@ -52,11 +60,15 @@ export const handler =  errorHandler(async (event: APIGatewayProxyEvent, ctx: Co
 			console.log(`DELETE /products/{id} called with ${productId}`);
 
 
+			//TODO verificar por que está voltando not found
 			const productDeleted = await productRepository.delete(productId);
 
 			if (!productDeleted) {
 				throw new NotFoundException(`Product ${productId} not found to delete.`, apiRequestId);
 			}
+
+			const eventResponse = await sendProductEvent(productDeleted, 'email3@email3.com.br', lambdaRequestId, ProductEventType.DELETED);
+			console.log(`Event response: ${eventResponse}`);
 
 			return {
 				statusCode: 200,
@@ -73,3 +85,19 @@ export const handler =  errorHandler(async (event: APIGatewayProxyEvent, ctx: Co
 	};
 
 });
+
+function sendProductEvent(product: Product, email: string, lambdaRequestId: string, eventType: ProductEventType) {
+	// TODO: standardization of logs here on function
+	const event: ProductEvent = {
+		product,
+		email,
+		eventType,
+		requestId: lambdaRequestId,
+	};
+
+	return lambdaClient.invoke({
+		FunctionName: productEventsFunctionName,
+		Payload: JSON.stringify(event),
+		InvocationType: 'RequestResponse'
+	}).promise();
+}
