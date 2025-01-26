17fadd0bbf9b4225d216f2b1edc0f26f261e3a07
FilipeTenedini <tenedinifilipe@gmail.com>
2025-01-26 16:23:07 -0300

    feat: get orders layer and products layer on orders app stack

--- lib/orders-app.stack.ts
+++ lib/orders-app.stack.ts
@@ -1,4 +1,5 @@
 import * as apiGateway from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as lambdaNodeJs from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as cdk from 'aws-cdk-lib';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
@@ -27,6 +28,15 @@ export class OrdersAppStack extends cdk.Stack {
 			readCapacity: 1, // QUANTAS REQUISIÇÕES POR SEGUNDO
 			writeCapacity: 1, // QUANTAS REQUISIÇÕES POR SEGUNDO
 		});
+
+
+		const ordersLayerArn = ssm.StringParameter.valueForStringParameter(this, 'OrdersLayerVersionArn');
+		const ordersLayer = lambda.LayerVersion.fromLayerVersionArn(this, 'OrdersLayerVersionArn', ordersLayerArn);
+
+		const productsLayerArn = ssm.StringParameter.valueForStringParameter(this, 'ProductsLayerVersionArn');
+		const productsLayer = lambda.LayerVersion.fromLayerVersionArn(this, 'ProductsLayerVersionArn', productsLayerArn);
+
+
 	}
 }
 
