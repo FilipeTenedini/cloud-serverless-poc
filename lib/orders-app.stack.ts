e288740d52829e68ffd57083d9bf049e13183193
FilipeTenedini <tenedinifilipe@gmail.com>
2025-01-27 09:28:10 -0300

    feat: add ordersHandler on orders app stack and grant write and read data to db

--- lib/orders-app.stack.ts
+++ lib/orders-app.stack.ts
@@ -11,6 +11,8 @@ interface OrdersAppStackProps extends cdk.StackProps {
 }
 
 export class OrdersAppStack extends cdk.Stack {
+	ordersHandler: lambdaNodeJs.NodejsFunction;
+
 	constructor(scope: Construct, id: string, props: OrdersAppStackProps) {
 		super(scope, id, props);
 
@@ -36,7 +38,31 @@ export class OrdersAppStack extends cdk.Stack {
 		const productsLayerArn = ssm.StringParameter.valueForStringParameter(this, 'ProductsLayerVersionArn');
 		const productsLayer = lambda.LayerVersion.fromLayerVersionArn(this, 'ProductsLayerVersionArn', productsLayerArn);
 
+		this.ordersHandler = new lambdaNodeJs.NodejsFunction(this, 'OrdersFunction', {
+			runtime: lambda.Runtime.NODEJS_20_X,
+			functionName:'OrdersFunction',
+			entry: 'lambda/orders/orders.function.ts',
+			handler: 'handler',
+			memorySize: 512,
+			timeout: cdk.Duration.seconds(5),
+			bundling: {
+				minify: true,
+				sourceMap: false,
+				nodeModules: [
+					'aws-xray-sdk-core'
+				]
+			},
+			environment: {
+				PRODUCTS_DB: props.productsDb.tableName,
+				ORDERS_DB: ordersDb.tableName,
+			},
+			layers: [ordersLayer, productsLayer],
+			tracing: lambda.Tracing.ACTIVE,
+			insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_119_0,
+		});
 
+		ordersDb.grantReadWriteData(this.ordersHandler);
+		props.productsDb.grantReadData(this.ordersHandler);
 	}
 }
 
