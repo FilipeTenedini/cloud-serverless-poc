2ba086d4c6caf1642031bd9d0c4372efe8a541e3
FilipeTenedini <tenedinifilipe@gmail.com>
2025-01-27 18:06:45 -0300

    feat: export orders stack

--- lib/index.ts
+++ lib/index.ts
@@ -2,3 +2,5 @@ export * from './ecommerce-api.stack';
 export * from './products-app.stack';
 export * from './product-app-layers.stack';
 export * from './events-db.stack';
+export * from './orders-app-layers.stack';
+export * from './orders-app.stack';
